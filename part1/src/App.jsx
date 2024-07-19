5c16a5df11e9d12c07fdd1b331de37a8b60c6d5f
LizethVictoria20 <lizvictoria754@gmail.com>
2024-07-19 15:29:38 -0500

    Pasando controladores de eventos a componentes hijos

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,49 +2,22 @@ import { useState } from "react";
 import Display from './components/Display'
 import Button from './components/Button'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
 
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [total, setTotal] = useState(0)
+  const [value, setValue] = useState(10)
 
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updateLeft = left + 1
-    setLeft(updateLeft)
-    setTotal(updateLeft + right)
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const updateRight = right + 1
-    setRight(updateRight)
-    setTotal(left + updateRight)
-  }
   return (
     <div>
-      {left}
-      <Button onClick={handleRightClick} text="Right"/>
-      <Button onClick={handleLeftClick} text="Left"/>
-      {right}
-      <History allClicks={allClicks}/>
+      {value}
+      <Button onClick={() => setToValue(1000)} text="thousand"/>
+      <Button onClick={() => setToValue(0)} text="reset"/>
+      <Button onClick={() => setToValue(value + 1)} text="increment"/>
     </div>
   )
 }
