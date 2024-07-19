2f20985943a717e4e8dd84edaccdbb9dd1b95915
LizethVictoria20 <lizvictoria754@gmail.com>
2024-07-19 13:23:27 -0500

    Un controlador de eventos es una función

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,30 +1,20 @@
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Total from "./components/Total";
+import { useState } from "react";
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
+  const [counter, setCounter] = useState(0);
+  const increaseByOne = () => {
+    setCounter(counter + 1);
   };
+  const setToZero = () => {
+    setCounter(0);
+  }
+
+
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      {counter}
+      <button onClick={increaseByOne}>Click me!</button>
+      <button onClick={setToZero}>Zero</button>
     </div>
   );
 };
