3d9822af41e5ba11a3440c68e4f2d66fc41d6f66
LizethVictoria20 <lizvictoria754@gmail.com>
2024-07-19 16:12:17 -0500

    1.6: unicafe, paso 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,25 +1,34 @@
-import { useState } from "react";
-import Display from './components/Display'
+import { useState } from 'react'
 import Button from './components/Button'
-
-
-
 const App = () => {
-  const [value, setValue] = useState(10)
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const setToValue = (newValue) => {
-    console.log('value now', newValue)
-    setValue(newValue)
+  const goodReview = () => {
+    setGood(good + 1)
+  }
+  const neutralReview = () => {
+    setNeutral(neutral + 1)
+  }
+  const badReview = () => {
+    setBad(bad + 1)
   }
 
   return (
     <div>
-      {value}
-      <Button onClick={() => setToValue(1000)} text="thousand"/>
-      <Button onClick={() => setToValue(0)} text="reset"/>
-      <Button onClick={() => setToValue(value + 1)} text="increment"/>
+    <h1>Give a feedback</h1>
+      <Button onClick={goodReview} text="Good"/>
+      <Button onClick={neutralReview} text="Neutral"/>
+      <Button onClick={badReview} text="Bad"/>
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      
     </div>
   )
 }
 
-export default App;
+export default App
