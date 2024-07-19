353245d390d7471a9dd039f56c73902eb3ff21e3
LizethVictoria20 <lizvictoria754@gmail.com>
2024-07-19 11:04:36 -0500

    1.5: Información del Curso paso 5

--- part1/src/components/Content.jsx
+++ part1/src/components/Content.jsx
@@ -3,9 +3,11 @@ import Part from "./Part";
 function Content({ parts }) {
   return (
     <>
-      {parts.map((part, index) => (
-        <Part key={index} part={part.name} exercises={part.exercises} />
-      ))}
+      {
+        parts.map((part, index) => (
+          <Part key={index} part={part.name} exercises={part.exercises} />
+        ))
+      }
     </>
   );
 }
