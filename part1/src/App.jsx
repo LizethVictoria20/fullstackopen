0083cfb20ad8d79ef4bfaa82ff474ccf065fd30b
LizethVictoria20 <lizvictoria754@gmail.com>
2024-07-19 18:11:42 -0500

    unicafe, paso 3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,12 +1,36 @@
 import { useState } from 'react'
 import Button from './components/Button'
+
+
+const Statistics = ({ good, neutral, bad, total}) => {
+  const average = good + neutral + bad / total
+  const positive = (100 * good) / total
+  if(average > 0 && positive > 0) {
+    return (
+      <>
+         <p>Average: {average}</p>
+         <p>Positive: {positive}%</p>
+      </>
+    )
+  }
+  else {
+    return (
+      <>
+         <p>Average: 0</p>
+         <p>Positive: 0%</p>
+      </>
+    )
+  }
+
+}
+
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [total, setTotal] = useState(0)
-  // const [average, setAverage] = useState(0)
 
   const goodReview = () => {
     const goodReview = good + 1
@@ -29,8 +53,7 @@ const App = () => {
     setTotal(totalReviews)
   }
 
-  const average = good + neutral + bad / total
-  const positive = (100 * good) / total
+
 
   return (
     <div>
@@ -43,8 +66,7 @@ const App = () => {
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
       <p>All: {total}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {positive}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} total={total}/>
     </div>
   )
 }
