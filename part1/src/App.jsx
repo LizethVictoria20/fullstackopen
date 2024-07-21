28bb9a3bca38c249c5095226ad7e490041ad95d4
LizethVictoria20 <lizvictoria754@gmail.com>
2024-07-21 12:43:49 -0500

    1.13*: anecdotes, paso 2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,111 +1,42 @@
-import { useState } from "react";
-import Button from "./components/Button";
-
-const StatisticsLine = ({ text, value }) => {
-  return (
-    <p>
-      {text} {value}
-    </p>
-  );
-};
-
-const Statistics = ({ good, neutral, bad, total }) => {
-  const average = good + neutral + bad / total;
-  const positive = (100 * good) / total;
-  return (
-    <>
-    <table>
-      <tr>
-        <td>
-        <StatisticsLine text="Good" value={good} />
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <StatisticsLine text="Neutral" value={neutral} />
-        </td>
-      </tr>
-      <td>
-        <tr>
-          <StatisticsLine text="Bad" value={bad} />
-        </tr>
-      </td>
-      <tr>
-        <td>
-          <StatisticsLine text="Average" value={average.toFixed(2)} />
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <StatisticsLine text="Positive" value={`${positive.toFixed(2)}%`} />
-        </td>
-      </tr>
-    </table>
-    </>
-  );
-};
-
-const GeneralInformation = ({ goodReview, neutralReview, badReview }) => {
-  return (
-    <div>
-      <h1>Give a feedback</h1>
-      <Button onClick={goodReview} text="Good" />
-      <Button onClick={neutralReview} text="Neutral" />
-      <Button onClick={badReview} text="Bad" />
-      <h2>Statistics</h2>
-    </div>
-  );
-};
+import { useState } from 'react'
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [total, setTotal] = useState(0);
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  const goodReview = () => {
-    const goodReview = good + 1;
-    setGood(goodReview);
-    const totalReviews = total + 1;
-    setTotal(totalReviews);
-  };
+  const [selected, setSelected] = useState(0)
 
-  const neutralReview = () => {
-    const neutralReview = neutral + 1;
-    setNeutral(neutralReview);
-    const totalReviews = total + 1;
-    setTotal(totalReviews);
-  };
+  const initialVotes = new Array(anecdotes.length).fill(0)
+  const [vote, setVote] = useState(initialVotes)
 
-  const badReview = () => {
-    const badReview = bad + 1;
-    setBad(badReview);
-    const totalReviews = total + 1;
-    setTotal(totalReviews);
-  };
-  if (good != 0 || neutral != 0 || bad != 0) {
-    return (
-      <div>
-        <GeneralInformation
-          goodReview={goodReview}
-          neutralReview={neutralReview}
-          badReview={badReview}
-        />
-        <Statistics good={good} neutral={neutral} bad={bad} total={total} />
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <GeneralInformation
-          goodReview={goodReview}
-          neutralReview={neutralReview}
-          badReview={badReview}
-        />
-        <p>No feedback given</p>
-      </div>
-    );
+
+  const handleClick = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
   }
-};
 
-export default App;
+  const handleVote = () => {
+    const newVote = [...vote]
+    newVote[selected] += 1
+    setVote(newVote)
+  }
+
+
+  return (
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <p>Has {vote[selected]} vote</p>
+      <button onClick={handleClick}>Next anecdotes</button>
+      <button onClick={handleVote}>Vote</button>
+    </div>
+  )
+}
+
+export default App
